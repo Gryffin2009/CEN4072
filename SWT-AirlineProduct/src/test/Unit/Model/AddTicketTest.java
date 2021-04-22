a29a7bb5f93a2d72db7bf286f4e275d05a54cef4
gortiz7941 <43448633+gortiz7941@users.noreply.github.com>
2021-04-22 12:56:46 -0400

    Commented AddressTest& AddTicketTest

--- SWT-AirlineProduct/src/test/Unit/Model/AddTicketTest.java
+++ SWT-AirlineProduct/src/test/Unit/Model/AddTicketTest.java
@@ -32,15 +32,46 @@ public class AddTicketTest {
 		ticket = new Ticket(id, flightid, custid, flightclass, price, seats, date);
 	}
 
-	// Tries to pass a valid street address to the Address class.
+	/*
+	 * Test Case ID: TC-T-8
+	 * Requirement ID/Description: T-5 Tickets shall include a valid number of seats. A valid number
+	 * 		of seats is between 0 and 10, exclusively.
+	 * Purpose: To test a valid seat value.
+	 * Test setup: Ticket object with the following values:
+	 * 		String id = AutoIDService.generateAutoID("ticket", "TO");
+	 *		String flightid = "";
+	 *		String custid = "";
+	 * 		String flightclass = "Business";
+	 * 		String price = "100";
+	 *		String seats = "1";
+	 *		String date = "2021-04-22";
+	 * Test Strategy: equivalence partitioning: ivalid <=0, valid 1-9, invalid >9
+	 * Input: int: 5
+	 * Expected Output: accepted seat value.
+	 */
 	@Test
 	@DisplayName("Valid seats")
-	void testPassportValid() {
+	void testValidSeat() {
 		Assertions.assertDoesNotThrow(() -> ticket.setNumSeats(5));
 	}
 
-	// Tries to pass an invalid street address to the Address class.
-	// No numbers
+	/*
+	 * Test Case ID: TC-T-9
+	 * Requirement ID/Description: T-5 Tickets shall include a valid number of seats. A valid number
+	 * 		of seats is between 0 and 10, exclusively.
+	 * Purpose: To test a invalid seat value.
+	 * Test setup: Ticket object with the following values:
+	 * 		String id = AutoIDService.generateAutoID("ticket", "TO");
+	 *		String flightid = "";
+	 *		String custid = "";
+	 * 		String flightclass = "Business";
+	 * 		String price = "100";
+	 *		String seats = "1";
+	 *		String date = "2021-04-22";
+	 * Test Strategy: equivalence partitioning: ivalid <=0, valid 1-9, invalid >9
+	 * Input: int: 50
+	 * Expected Output: Seat value not accepted.
+	 */
 	@Test
 	@DisplayName("Invalid seats, too many")
 	void testSeatsInvalidNoNumbers() throws InvalidTicketInputException {
@@ -51,19 +82,68 @@ public class AddTicketTest {
 
 	// Tries to pass an invalid street address to the Address class.
 	// Only numbers
+	/*
+	 * Test Case ID: TC-T-10
+	 * Requirement ID/Description: T-5 Tickets shall include a valid number of seats. A valid number
+	 * 		of seats is between 0 and 10, exclusively.
+	 * Purpose: To test a invalid seat value.
+	 * Test setup: Ticket object with the following values:
+	 * 		String id = AutoIDService.generateAutoID("ticket", "TO");
+	 *		String flightid = "";
+	 *		String custid = "";
+	 * 		String flightclass = "Business";
+	 * 		String price = "100";
+	 *		String seats = "1";
+	 *		String date = "2021-04-22";
+	 * Test Strategy: equivalence partitioning: ivalid <=0, valid 1-9, invalid >9
+	 * Input: int:  0
+	 * Expected Output: Seat value not accepted.
+	 */
 	@Test
 	@DisplayName("Invalid seats, 0")
-	void testPassportInvalidOnlyNumbers() throws InvalidTicketInputException {
+	void testInvalidSeat0() throws InvalidTicketInputException {
 		Exception e = Assertions.assertThrows(InvalidTicketInputException.class, () -> ticket.setNumSeats(0));
 		Assertions.assertEquals("Invalid Number of Seats", e.getMessage());
 	}
 
+	/*
+	 * Test Case ID: TC-T-13
+	 * Requirement ID/Description: T-3 Tickets shall have a price. The price shall consist of numerical input.
+	 * Purpose: To test a valid date value.
+	 * Test setup: Ticket object with the following values:
+	 * 		String id = AutoIDService.generateAutoID("ticket", "TO");
+	 *		String flightid = "";
+	 *		String custid = "";
+	 * 		String flightclass = "Business";
+	 * 		String price = "100";
+	 *		String seats = "1";
+	 *		String date = "2021-04-22";
+	 * Test Strategy: positive testing
+	 * Input:  2021-04-22
+	 * Expected Output: accepted date value.
+	 */
 	@Test
 	@DisplayName("Valid Date, proper format")
 	void testValidDate() throws InvalidTicketInputException {
 		Assertions.assertDoesNotThrow(() -> ticket.setDate("2021-04-22"));
 	}
 
+	/*
+	 * Test Case ID: TC-T-14
+	 * Requirement ID/Description: T-3 Tickets shall have a price. The price shall consist of numerical input.
+	 * Purpose: To test a invalid date value.
+	 * Test setup: Ticket object with the following values:
+	 * 		String id = AutoIDService.generateAutoID("ticket", "TO");
+	 *		String flightid = "";
+	 *		String custid = "";
+	 * 		String flightclass = "Business";
+	 * 		String price = "100";
+	 *		String seats = "1";
+	 *		String date = "2021-04-22";
+	 * Test Strategy: positive testing
+	 * Input: "HELLO"
+	 * Expected Output: Date value not accepted.
+	 */
 	@Test
 	@DisplayName("Invalid Date, improper format")
 	void testInvalidDate() throws InvalidTicketInputException {
@@ -71,12 +151,44 @@ public class AddTicketTest {
 		Assertions.assertEquals("Invalid date. Improper format.", e.getMessage());
 	}
 
+	/*
+	 * Test Case ID: TC-T-5
+	 * Requirement ID/Description: T-3 Tickets shall have a price. The price shall consist of numerical input.
+	 * Purpose: To test a valid price value.
+	 * Test setup: Ticket object with the following values:
+	 * 		String id = AutoIDService.generateAutoID("ticket", "TO");
+	 *		String flightid = "";
+	 *		String custid = "";
+	 * 		String flightclass = "Business";
+	 * 		String price = "100";
+	 *		String seats = "1";
+	 *		String date = "2021-04-22";
+	 * Test Strategy: positive testing
+	 * Input: int:  100
+	 * Expected Output: Seat value not accepted.
+	 */
 	@Test
 	@DisplayName("Valid price, numeric")
 	void testValidPrice() throws InvalidTicketInputException {
 		Assertions.assertDoesNotThrow(() -> ticket.setPrice("100"));
 	}
 
+	/*
+	 * Test Case ID: TC-T-6
+	 * Requirement ID/Description: T-3 Tickets shall have a price. The price shall consist of numerical input.
+	 * Purpose: To test a valid price value.
+	 * Test setup: Ticket object with the following values:
+	 * 		String id = AutoIDService.generateAutoID("ticket", "TO");
+	 *		String flightid = "";
+	 *		String custid = "";
+	 * 		String flightclass = "Business";
+	 * 		String price = "100";
+	 *		String seats = "1";
+	 *		String date = "2021-04-22";
+	 * Test Strategy: positive testing
+	 * Input: int:  100
+	 * Expected Output: Seat value not accepted.
+	 */
 	@Test
 	@DisplayName("Invalid price, alphanumeric")
 	void testInvalidPrice() throws InvalidTicketInputException {
--- SWT-AirlineProduct/src/test/Unit/Model/AddressTest.java
+++ SWT-AirlineProduct/src/test/Unit/Model/AddressTest.java
@@ -13,14 +13,14 @@ import Model.Address.InvalidAddressInputException;
 public class AddressTest {
 
 	Address address;
-	
-	
+
+
 	String streetAddress = "123 Main Street";
 	String city = "Fort Myers";
 	String state = "Florida";
 	String zipCode = "33913";
 	String country = "United States";
-	
+
 	// Positive test
 	@BeforeEach
 	void beforeEach() throws IOException, InvalidAddressInputException {
@@ -31,63 +31,142 @@ public class AddressTest {
 			address = new Address(streetAddress, city, state, zipCode, country);
 		});
 	}
-	
-	// Tries to pass a valid street address to the Address class.
+
+	/* Tries to pass a valid street address to the Address class.
+	 * Test Case ID: AddressTC-1
+	 * Requirement ID/Description: A-1 An address shall contain a valid street address. A valid street
+	 * 		address shall contain a building number followed by a street name. It may optionally end in
+	 * 		an apartment number.
+	 * Purpose: To test a valid street address input
+	 * Test setup: an address object is created with the following values: 123 Main Street, Fort Myers,
+	 * 		Florida, 33913, United States
+	 * Test Strategy: positive testing
+	 * Input: Data type String: 1234 main street
+	 * Expected Output: Accepts the input.
+	 */
 	@Test
 	@DisplayName("Address, valid")
 	void testAddressValid() {
 		Assertions.assertDoesNotThrow(() -> address.setStreetAddress("1234 main street"));
 	}
 
+
 	// Tries to pass an invalid street address to the Address class.
 	// No numbers
+	/*
+	 * Test Case ID: AddressTC-2
+	 * Requirement ID/Description: A-1 An address shall contain a valid street address. A valid street
+	 * 		address shall contain a building number followed by a street name. It may optionally end in
+	 * 		an apartment number.
+	 * Purpose: To test an invalid street address.
+	 * Test setup:  an address object is created with the following values: 123 Main Street, Fort Myers,
+	 * 		Florida, 33913, United States
+	 * Test Strategy: Negative Testing
+	 * Input: string: main street
+	 * Expected Output: Does not accept the input.
+	 */
 	@Test
 	@DisplayName("Address, no numbers (Invalid)")
 	void testAddressInvalidNoNumbers() throws InvalidAddressInputException, Exception {
-		InvalidAddressInputException e = Assertions.assertThrows(InvalidAddressInputException.class, 
+		InvalidAddressInputException e = Assertions.assertThrows(InvalidAddressInputException.class,
 				() -> address.setStreetAddress("main street"));
 		Assertions.assertEquals("Invalid street address", e.getMessage());
 	}
 
 	// Tries to pass an invalid city to the Address class.
 	// Only numbers
+	/*
+	 * Test Case ID:AddressTC-3
+	 * Requirement ID/Description: A-2 An address shall contain a valid city. A valid city shall
+	 * 		contain only alphabetical input.
+	 * Purpose: To test an invalid city.
+	 * Test setup:   an address object is created with the following values: 123 Main Street, Fort Myers,
+	 * 		Florida, 33913, United States
+	 * Test Strategy: negative testing
+	 * Input: empty string.
+	 * Expected Output: Does not accept the input.
+	 */
 	@Test
 	@DisplayName("Address city, empty (Invalid))")
 	void testCityInvalid() throws InvalidAddressInputException, Exception {
 		InvalidAddressInputException e = Assertions.assertThrows(InvalidAddressInputException.class, () -> address.setCity(""));
 		Assertions.assertEquals("Invalid city", e.getMessage());
 	}
-	
+
 
 	// Tries to pass an invalid state to the Address class.
 	// Only numbers
+	/*
+	 * Test Case ID: Address TC-4
+	 * Requirement ID/Description: A-3 An address shall contain a valid state. A valid state shall
+	 * 		contain only alphabetical input.
+	 * Purpose: To test an invalid city.
+	 * Test setup:   an address object is created with the following values: 123 Main Street, Fort Myers,
+	 * 		Florida, 33913, United States
+	 * Test Strategy: negative testing
+	 * Input: empty string.
+	 * Expected Output: Does not accept the input.
+	 */
 	@Test
 	@DisplayName("Address state, empty (Invalid))")
 	void testStateInvalid() throws InvalidAddressInputException, Exception {
 		InvalidAddressInputException e = Assertions.assertThrows(InvalidAddressInputException.class, () -> address.setState(""));
 		Assertions.assertEquals("Invalid state", e.getMessage());
 	}
-	
+
 
 	// Tries to pass an invalid zipCode to the Address class.
 	// Only numbers
+	/*
+	 * Test Case ID: Address TC-5
+	 * Requirement ID/Description: A-4 An address shall contain a valid zip code. A valid zip code
+	 * 		shall only contain numeric input.
+	 * Purpose: To test an invalid zip code.
+	 * Test setup:   an address object is created with the following values: 123 Main Street, Fort Myers,
+	 * 		Florida, 33913, United States
+	 * Test Strategy: negative testing
+	 * Input: empty string.
+	 * Expected Output: Does not accept the input.
+	 */
 	@Test
 	@DisplayName("Address zipCode, empty (Invalid))")
 	void testZipCodeInvalid() throws InvalidAddressInputException, Exception {
 		InvalidAddressInputException e = Assertions.assertThrows(InvalidAddressInputException.class, () -> address.setZipCode(""));
 		Assertions.assertEquals("Invalid zipCode", e.getMessage());
 	}
-	
+
 	// Tries to pass an invalid country to the Address class.
 	// Only numbers
+	/*
+	 * Test Case ID: Address TC-6
+	 * Requirement ID/Description: A-5 An address shall contain a valid zip code. A valid zip code
+	 * 		shall only contain numeric input.
+	 * Purpose: To test an invalid country.
+	 * Test setup:   an address object is created with the following values: 123 Main Street, Fort Myers,
+	 * 		Florida, 33913, United States
+	 * Test Strategy: negative testing
+	 * Input: empty string.
+	 * Expected Output: Does not accept the input.
+	 */
 	@Test
 	@DisplayName("Address country, empty (Invalid))")
 	void testCountryInvalid() throws InvalidAddressInputException, Exception {
 		InvalidAddressInputException e = Assertions.assertThrows(InvalidAddressInputException.class, () -> address.setCountry(""));
 		Assertions.assertEquals("Invalid country", e.getMessage());
 	}
-	
-	// Ensure the printed string contains all 
+
+	// Ensure the printed string contains all
+	/*
+	 * Test Case ID: Address TC-7
+	 * Requirement ID/Description: A-6 An address shall contain the following components: street
+	 * 		address, city, state, zip code, and country.
+	 * Purpose: To ensure address object contains all components stated in A-6.
+	 * Test setup:   an address object is created with the following values: 123 Main Street, Fort Myers,
+	 * 		Florida, 33913, United States
+	 * Test Strategy: positive testing
+	 * Input: empty string.
+	 * Expected Output: Address object contains all components.
+	 */
 	@Test
 	@DisplayName("Address, toString()")
 	void testToString() {
@@ -98,6 +177,6 @@ public class AddressTest {
 		Assertions.assertTrue(str.contains(zipCode));
 		Assertions.assertTrue(str.contains(country));
 	}
-	
-	
+
+
 }
